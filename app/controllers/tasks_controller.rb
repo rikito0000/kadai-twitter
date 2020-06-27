1bb58d8ad8fb022fab049d43cc7f473a54811855
EC2 Default User <ec2-user@ip-172-31-52-127.ec2.internal>
2020-06-27 05:11:42 +0000

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+   before_action :require_user_logged_in
    before_action :set_task, only: [:show, :edit, :update, :destroy]
+   
+   
   def index
     @tasks = Task.all.page(params[:page]).per(3)
   end
@@ -12,7 +15,7 @@ class TasksController < ApplicationController
   end
   
   def create
-     @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
@@ -51,6 +54,13 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
   
